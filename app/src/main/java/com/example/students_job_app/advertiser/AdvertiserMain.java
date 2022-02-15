63ae26555ef5dab5e65c0da3e2a8f24eb3451178
odaykrayem <0day.krayem0@gmail.com>
2022-02-15 17:32:44 +0200

    second by Oday

---
+++ app/src/main/java/com/example/students_job_app/advertiser/AdvertiserMain.java
@@ -0,0 +1,16 @@
+package com.example.students_job_app.advertiser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.students_job_app.R;
+
+public class AdvertiserMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertiser_main);
+    }
+}
