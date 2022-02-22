13784eb484d931f90113be291a8bf6841c0cf04c
odaykrayem <0day.krayem0@gmail.com>
2022-02-22 16:24:50 +0200

    fifth by Oday: designs finished

--- app/src/main/java/com/example/students_job_app/student/fragments/StudentProfileFragment.java
+++ app/src/main/java/com/example/students_job_app/student/fragments/StudentProfileFragment.java
@@ -10,9 +10,9 @@ import android.view.ViewGroup;
 
 import com.example.students_job_app.R;
 
-public class ProfileFragment extends Fragment {
+public class StudentProfileFragment extends Fragment {
 
-    public ProfileFragment() {
+    public StudentProfileFragment() {
         // Required empty public constructor
     }
 
@@ -20,7 +20,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_student_profile, container, false);
     }
 
 
